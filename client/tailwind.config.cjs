2223186c1b296369dfe6593acf3399604850b4a7
Phoebe Ogallo <adhiambophoebe19@gmail.com>
2022-11-24 19:49:37 -0800

    our configuration files

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'our-blue': '#4F72A7'
+      }
+    },
+  },
+  plugins: [],
+}
