db121644459590d299a0df2ac615c1587e420fb0
Newton Ombese <newtonombese1@gmail.com>
2022-11-25 06:06:39 +0300

    getting my socials

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,31 +3,31 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <h5>John Oketch &copy; 2022</h5>
+      <h5>Newton Ombese & Phoebe Ogallo &copy; 2022</h5>
       <div className="socials">
         <a
-          href="https://www.instagram.com/john_mark_69/"
+          href="https://www.instagram.com/__.bofulo.mkate.__/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FiUser />
         </a>
         <a
-          href="https://github.com/AWITI0301"
+          href="https://github.com/NewtonOmbese"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FiGithub />
         </a>
         <a
-          href="linkedin.com/in/john-awiti-4b2110208/"
+          href="https://www.linkedin.com/in/newton-ombese-570862210/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FiLinkedin />
         </a>
         <a
-          href="https://www.facebook.com/profile.php?id=100008208971720"
+          href="https://www.facebook.com/profile.php?id=100085694547730"
           target="_blank"
           rel="noopener noreferrer"
         >
